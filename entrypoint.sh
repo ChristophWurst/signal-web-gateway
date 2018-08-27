8e355e0aba3a1bbeb18d11e00291d10e69c59e75
morph027 <stefan.heitmueller@gmx.com>
2018-08-27 18:30:55 +0200

    add CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,8 @@ stop() {
   echo "[ERROR] It looks like your Signal identity has not been setup properly, please follow instructions on how to register an account"
 }
 
-[ -f /signal/.storage/identity/identity_key ] && start || stop
+if [ -f /signal/.storage/identity/identity_key ]; then
+  start
+else
+  stop
+fi
